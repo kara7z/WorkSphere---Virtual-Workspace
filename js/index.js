0d15f671e1f743f4b7154dd1db1d9c8e06ec84cf
kara7z <kara.ossama12@gmail.com>
2025-11-20 12:31:46 +0100

    Members tab

--- js/index.js
+++ js/index.js
@@ -4,8 +4,12 @@ const input = document.getElementById('imageUrlInput');
 const preview = document.getElementById('previewImage');
 const addWorker = document.getElementById('add');
 const form = document.getElementById('Add-Form');
+const AddExpArea =document.querySelector('.add-Exp')
 const SaveWorker = document.getElementById('Save-Worker');
 const Cancel = document.getElementById('Cancel');
+const AddExp = document.getElementById('Add-Exp');
+const AddClose = document.getElementById('AddClose');
+const backdrop = document.querySelector('.backdrop');
 let SelectedItem = null;
 
 
@@ -22,18 +26,44 @@ function updatePreview() {
     if (input.value.trim() !== "") {
         preview.src = input.value;
     } else {
-        preview.src = ""; 
+        preview.src = "images/Default_pfp.jpg"; 
     }
 }
 updatePreview();
-Cancel.addEventListener('click',(e)=>{
-    e.preventDefault()
-    form.style.display ="none";
+
+function EventHide(e,area){
+    e.addEventListener('click',()=>{
+        area.style.display ='none'
+    })
+}
+function EventShow(e,area){
+    e.addEventListener('click',()=>{
+        area.style.display ='flex';
+    })
+}
+EventHide(AddClose,AddExpArea);
+EventShow(AddExp,AddExpArea);
+
+
+Cancel.addEventListener('click',()=>{
+    form.style.display = "none";
+    backdrop.style.display = "none";       
+    document.body.classList.remove('modal-open');
+    AddExpArea.style.display = "none";
+
 })
 addWorker.addEventListener('click',(e)=>{
-    e.preventDefault()
+    e.preventDefault();
     form.style.display ="flex";
+    backdrop.style.display = "block"
+    document.body.classList.add('modal-open');
 })
+backdrop.addEventListener('click', () => {
+    form.style.display = "none";
+    backdrop.style.display = "none";
+    document.body.classList.remove('modal-open');
+    AddExpArea.style.display = "none";
+});
 input.addEventListener('input', updatePreview);
 
 Workspace.addEventListener('click', (e) => {
@@ -50,3 +80,6 @@ Workspace.addEventListener('click', (e) => {
 
     roomInfo.textContent =roomDescriptions[clicked.id];
 });
+const array = [1,3,5,7,8]
+const newArray= array.map((e)=>e*2).filter(e=>e>5)
+console.log(newArray);
